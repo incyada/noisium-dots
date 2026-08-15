228479fa5cf9624c15d2a92e94785f246aadd88d
Incyada <incyada@protonmail.com>
2026-08-15 19:36:49 -0400

    think i got a plan now, so now all thats left, is to put it in practice

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 # things to do:
-# 1. update repos and check which package manager is on the system (paru, yay, dnf, or apt)
-# 1. auto-check needed packages first
-# 2. install extensions needed ( do want to support the projects at hand, although, im not sure how easy it will be)
-# 3. auto-create any folders that arent there for the installation
+# 1. backup all replaced files
+# 2. update repos and check which package manager is on the system (paru, yay, dnf, or apt)
+# 3. auto-check needed packages first
+# 4. install extensions needed ( do want to support the projects at hand, although, im not sure how easy it will be)
+# 5. auto-create any folders that arent there for the installation
 # # folders needed: ~/.local/share/icons/hicolor/512x512/app, ~/.local/share/aurorae/themes, ~/.local/share/kwin/scripts
-# 4. replace instances of my name (incyada) to $USER
-# 5. lots of manual copy-pasting
+# 6. replace instances of my name (incyada) to $USER
+# 7. check if the device has battery, why? cause not everyone has a laptop
+# 8. lots of manual copy-pasting
 
 # you can replace strings by changing its value later down the line, just like python, thank god
